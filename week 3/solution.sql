9f5a5c9a06c1c19cb7883eb5dfb3163755f572bd
Lev Chechulin <l.e.chechulin@gmail.com>
2019-07-10 22:49:48 +0200

    added queries

---
+++ week 3/solution.sql
@@ -0,0 +1,66 @@
+CREATE TABLE query1
+	(
+	name TEXT,
+	moviecout BIGINT
+	);
+	
+FROM movies SELECT
+	;
+
+CREATE TABLE query2
+	(
+	name TEXT,
+	rating NUMERIC
+	);
+	
+CREATE TABLE query3
+	(
+	title TEXT,
+	countofratings BIGINT
+	);
+
+CREATE TABLE query4
+	(
+	movieid INTEGER,
+	title TEXT
+	);
+
+CREATE TABLE query5
+	(
+	title TEXT,
+	average NUMERIC
+	);
+
+CREATE TABLE query6
+	(
+	average NUMERIC
+	);
+
+CREATE TABLE query7
+	(
+	average NUMERIC
+	);
+	
+CREATE TABLE query8
+	(
+	average NUMERIC
+	);
+
+CREATE TABLE query9
+	(
+	movieid NUMERIC
+	rating NUMERIC
+	);
+
+CREATE TABLE query10
+	(
+	title TEXT
+	);
+
+CREATE TABLE query11
+	(
+	movieid1 INTEGER,
+	movieid2 INTEGER,
+	sim NUMERIC
+	);
+
